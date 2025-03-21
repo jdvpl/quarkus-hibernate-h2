b6b226f30ecf763e5e72fbec1da8427aba407b11
jdvpl <juanda554242@gmail.com>
2025-03-20 21:29:12 -0500

    first commit

---
+++ src/test/java/org/jdvplfullstackquarkushibernateIT.java
@@ -0,0 +1,8 @@
+package org;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class jdvplfullstackquarkushibernateIT extends jdvplfullstackquarkushibernateTest {
+    // Execute the same tests but in packaged mode.
+}
